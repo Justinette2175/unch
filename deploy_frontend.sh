dc045406388b65503e6c0ef6f52527f3f5b4fd10
Marc Burns <m4burns@uwaterloo.ca>
2017-11-26 08:16:15 -0500

    whoops uncomment

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
-#( cd font-end-template && npm install && npm run build )
+( cd font-end-template && npm install && npm run build )
 rsync -av --delete --filter 'protect .gitignore' font-end-template/build/* srv/
