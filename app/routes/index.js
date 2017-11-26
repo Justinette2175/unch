34d3e04f1b04357a9390a4cfbb0dfe2f07924745
jason <jason.gendreau@gmail.com>
2017-11-26 07:41:06 -0500

    Remove test person creation in index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,32 +5,7 @@ var Person = require('../models/person');
 var ContactInfo = require('../models/contactInfo');
 
 router.get('/', function(req, res) {
-    var testName = 'test';
-
-    var newPerson = {
-      name: testName,
-      story: 'had to run away'
-    };
-
-    var contactInfo = {
-            address: '30 street',
-            phone: '123-123-12312',
-            email: 'random@email.com',
-            socialMedia: 'social media test',
-    };
-
-    Person.createPerson(newPerson, contactInfo, function(err, response, msg) {
-        if(err) {
-          throw err;
-        } else if (msg) {
-          res.json('message: ' + msg);
-        } else {
-          Person.getByName(testName, function(err, person) {
-              if(err) throw err;
-              res.json(person);
-          });
-        }
-    });
+    res.send('hello')
 });
 
 module.exports = router;
